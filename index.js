4b32af73855000945f9dbec4062f21eb7b31bcf1
Bekov28 <orziqulov_berdiyor@mail.ru>
2024-10-08 20:16:04 +0900

    some practice

--- index.js
+++ index.js
@@ -4,11 +4,55 @@
 // console.log(__filename); - faylning path yo'lini ko'rsatadi;
 // console.log(process);
 
-// const fs = require("fs");
+// Path Module:
 
 const path = require("path");
 // console.log(path.basename(__filename)); - faylning to'liq nomini ko'rsatadi
 // console.log(path.extname(__filename)); - faylning kengaytmasini korsatadi -> .js
 // const ImageUrl = "salom.jpg";
 // console.log(path.extname(ImageUrl));
-console.log(path.join());
+// console.log(path.join(__dirname, "text.txt")); -> papka pathni yaratish, fayl emas!
+
+// Papka yaratish
+
+const fs = require("fs");
+const { log } = require("util");
+// fs.mkdir - buyruq beradi, lekin natijani kutmay, keyingisiga o'tib ketadi;
+// fs.mkdirSync -buyruq beradi va natijani kutadi;
+
+// Papka yaratish
+
+// fs.mkdir(path.join(__dirname, "Hello"), {}, (err) => {
+//   console.log(err);
+// });
+
+// Papka o'chirish
+
+// fs.rmdir(path.join(__dirname, "Hello"), (err) => {
+//   if (err) {
+//     throw err;
+//   }
+// });
+
+// Fayl yaratish
+
+// fs.writeFile(path.join(__dirname, "image.jpg"), "Image", (err) => {
+//   if (err) {
+//     throw err;
+//   }
+// });
+
+// fs.writeFile(path.join(__dirname, "text.txt"), "Salom", (err) => {
+//   if (err) {
+//     throw err;
+//   }
+// });
+
+// Faylni o'qish
+
+// fs.readFile(path.join(__dirname, "text.txt"), (err, data) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(data);
+// });
