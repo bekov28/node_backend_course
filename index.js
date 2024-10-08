d5aca2917f3851d43627b290dd36c6a5381bada1
Bekov28 <orziqulov_berdiyor@mail.ru>
2024-10-08 19:39:07 +0900

    node_fundamentals

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log(1);
+
+// console.log(__dirname);  - papkaning path - yo'lini ko'rsatadi;
+// console.log(__filename); - faylning path yo'lini ko'rsatadi;
+// console.log(process);
+
+// const fs = require("fs");
+
+const path = require("path");
+// console.log(path.basename(__filename)); - faylning to'liq nomini ko'rsatadi
+// console.log(path.extname(__filename)); - faylning kengaytmasini korsatadi -> .js
+// const ImageUrl = "salom.jpg";
+// console.log(path.extname(ImageUrl));
+console.log(path.join());
