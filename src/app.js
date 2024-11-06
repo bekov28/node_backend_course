689b666f8ee7d321543a7705313d166c116a37fc
Bekov28 <orziqulov_berdiyor@mail.ru>
2024-11-07 01:12:26 +0900

    connecting todo with front-end

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import { Routes } from "./routes/index.js";
 import { PORT } from "./utils/secrets.js";
 import { MONGODB_CONNECT } from "./utils/database.config.js";
+import cors from "cors";
 
 const server = express();
 
 void MONGODB_CONNECT();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true })); // this library is to receive data through form element;
+server.use(cors({}));
 
 Routes.forEach((item) => {
   server.use(item.path, item.router);
--- src/utils/secrets.js
+++ src/utils/secrets.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 3032;
+export const PORT = process.env.PORT || 3031;
 export const ENVIRONMENT = process.env.ENVIRONMENT;
 export const REG_KEY = process.env.REG_KEY;
 export const DB_URL = process.env.DB_URL;
