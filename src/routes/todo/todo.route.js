0108c3f85fda611b040736b3bf8e4a911840e759
Bekov28 <orziqulov_berdiyor@mail.ru>
2024-11-09 20:14:36 +0900

    JetBrains Mono Font and other fixes

--- src/routes/todo/todo.route.js
+++ src/routes/todo/todo.route.js
@@ -8,9 +8,31 @@ import {
   todoAdd,
   updateTodo,
 } from "../../controllers/todo/todo.controller.js";
-router;
 
-router.post("/add", todoAdd);
+// How to create middleware
+
+// const test = (req, res, next) => {
+//   console.log(req.body);
+//   next();
+// };
+// router.post("/add", test, todoAdd);  -> usage of Middleware
+
+// In case the user didnt fill out title and sent a request => //
+
+const test = (req, res, next) => {
+  const { title, desc } = req.body;
+
+  if (typeof title !== 'string' || title === "", title.length >= 5) {
+    return res.status(400).json({success: false, msg: "Title must be a string and not empty!"})
+  }
+  if (typeof desc !== 'string' || desc === "") {
+    return res.status(400).json({success: false, msg: "Desc must be a string and not empty!"})
+  }
+
+  next();
+};
+
+router.post("/add", test, todoAdd);
 
 router.put("/edit/:id", updateTodo);
 
